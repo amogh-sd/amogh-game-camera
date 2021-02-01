46483b1743d284de1df64aabb58a5aabab7f32a1
Amogh Dhavalikar <amoghdhavalikar@gmail.com>
2021-02-01 13:01:58 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -27,7 +27,7 @@ class Form {
       player.index = playerCount;
       player.update();
       player.updateCount(playerCount);
-      this.greeting.html("Hello " + player.name)
+      this.greeting.html("Hello " + player.name + "the game shall start soon")
       this.greeting.position(displayWidth / 2 - 70, displayHeight / 4);
     });
 
